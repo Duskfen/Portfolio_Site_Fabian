d867a66119c35bba7453f6a9be12803cfadf7f87
Gabriel Lugmayr <g.lugmayr@htlkrems.at>
2021-04-12 16:40:59 +0200

    fix for mobile (safari) -> footer was hidden because of the unecessary thick safari bar...

--- PorfolioFabian/dist/templates/styles.cfc2ba89.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{181:function(n,o,c){},182:function(n,o,c){},183:function(n,o,c){},184:function(n,o,c){},25:function(n,o,c){},26:function(n,o,c){},83:function(n,o,c){},84:function(n,o,c){},85:function(n,o,c){},86:function(n,o,c){},87:function(n,o,c){},88:function(n,o,c){},90:function(n,o,c){},96:function(n,o,c){}}]);
